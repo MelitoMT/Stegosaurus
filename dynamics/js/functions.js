8611c1e007e1c446b7743dd9487a53e589ca92c1
Carlos-V24 <vihcprepa6@comunidad.unam.mx>
2021-02-17 20:32:35 -0600

    FEAT: Multiples añadidos a preguntas y almacenamiento

--- dynamics/js/functions.js
+++ dynamics/js/functions.js
@@ -9,6 +9,34 @@ function cartaHover(opt,element){
         $("#modoDeJuegoForm").submit();
     })
 }
+// Utilizar esto para guardado de partida
+function checkName(nom) {
+  var listUsers = JSON.parse(localStorage.getItem("juagadore"));
+  for (var i = 0; i < listUsers.length; i++) {
+    var validName = true;
+    if (listUsers[i].name == nom) {
+      validName = false;
+    }
+  }
+  return validName;
+}
+function guardarUsuario(nom, contr) {
+  var listUsers = JSON.parse(localStorage.getItem("usuario"));
+  var newUser = {
+    name: nom,
+    password: contr,
+    record: "",
+    victorias: "0",
+    derrotas: "0",
+  };
+  if (users == null) {
+    var Usuarios = [newUser];
+  }else{
+    users.push(newUser)
+  }
+  localStorage.setItem("usuario", JSON.stringify(users));
+}
+
 function contIndex(ruta){
     fetch(ruta)
         .then((response)=>{
@@ -20,7 +48,7 @@ function contIndex(ruta){
             $("#cont").html(text);
             if(ruta = "./statics/menu.html"){
                 $("body").css("background","var(--gradienteGris)");
-            }         
+            }
         })
         .catch(error => {
             console.error('Fallo al obtener el contenido', error);
@@ -31,6 +59,6 @@ function generarFrasesCarga(){
     var frasesCarga = ['Elige sabiamente tu personaje...','Enseñando magia al Cangumago...', 'Michibot escribiendo su primer "Hola Mundo...','Eligiendo preguntas...' ];
     setInterval(()=>{
         var i = Math.floor(Math.random() * Math.floor(frasesCarga.length + 1));
-        $("#frases").html(frasesCarga[i]);    
+        $("#frases").html(frasesCarga[i]);
     },600);
 }
--- dynamics/js/index.js
+++ dynamics/js/index.js
@@ -7,6 +7,20 @@ $(document).ready(()=>{
         title.css("display","none");
         $("#footer").css("display","block");
         contIndex('./statics/menu.html');
-    
     }, 4000);
-}) 
+    // Verificacion almacenamiento local
+    if (typeof(Storage) !== "undefined") {
+      console.log("Si podemos");
+    } else {
+      console.log("No podmeos")
+    }
+    // Verificacion lectura archivos
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+      console.log("Si podemos");
+    } else {
+      alert('The File APIs are not fully supported in this browser.');
+    }
+    var preguntas = JSON.parse(preguntas);
+    // Devuelve el objeto con todas las preguntas
+    console.log(mydata);
+})
