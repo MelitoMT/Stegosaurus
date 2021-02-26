6ea0eaf1261738af6670b0f0588dede5bc0ae715
Carlos-V24 <vihcprepa6@comunidad.unam.mx>
2021-02-25 22:45:55 -0600

    FEAT: Se añadio el obtener pregunta

--- questions/preguntas.js
+++ questions/preguntas.js
@@ -1,41 +1,99 @@
-//var tsv is the TSV file with headers
-function tsvJSON(tsv){
+// La variable es el texto del tsv
+function tsvOBJ(tsv){
 
-  var lines=tsv.split("\n");
+  var lines=tsv.split("\n"); //Los pera por salto de linea
 
-  var result = [];
+  var result = []; //arreglo que se decolvera
 
-  var headers=lines[0].split("\t");
-
-  for(var i=1;i<lines.length;i++){
+  var headers=lines[0].split("\t"); //La primera linea la utiliza para guardar y asignar los titulos
+  for (var i = 0; i < headers.length; i++) {
+    headers[i] = headers[i].replace(/\r/g, '');//Elimina salto de linea no buscado
+  }
+  for(var i=1;i<lines.length;i++){ //Coniverte el restro de filas en objetos
 
 	  var obj = {};
-	  var currentline=lines[i].split("\t");
+	  var currentline=lines[i].split("\t"); //Separa los distintos valores por tabulacion
 
 	  for(var j=0;j<headers.length;j++){
-		  obj[headers[j]] = currentline[j];
+		  obj[headers[j]] = currentline[j];//Crea un objeto de cada linea
 	  }
 
-	  result.push(obj);
+	  result.push(obj);//guarda el objeto en el arreglo
 
   }
-
-  //return result; //JavaScript object
-  return JSON.stringify(result); //JSON
+  return result;//devuelve el arreglo
 }
 
-var nombreArchivoCSV = "Preguntas_computacion - Hoja 1"
+function obtenerCategorias(arr){
+  var categorias = [];
+  for (var i = 0; i < arr.length; i++) {
+    var agregar = true;
+    for (var j = 0; j < categorias.length; j++) {
+      if (arr[i].Categoria == categorias[j]) {
+        agregar =false;
+      }
+    }
+    if (agregar) {
+      categorias.push(arr[i].Categoria)
+    }
+  }
+  return categorias;
+}
+function obtenerDificultades(arr){
+  var dificultades = [];
+  for (var i = 0; i < arr.length; i++) {
+    var agregar = true;
+    for (var j = 0; j < dificultades.length; j++) {
+      if (arr[i].Dificultad == dificultades[j]) {
+        agregar =false;
+      }
+    }
+    if (agregar) {
+      dificultades.push(arr[i].Dificultad)
+    }
+  }
+  return dificultades;
+}
+// obtenerCategorias(window.preguntas);
+function generarPregunta(cat, dif) {
+  // Regresa el valor de la funcion busquedaPreg
+  return busquedaPreg (cat, dif, window.preguntas, (dificultad, arreglo, callback)=>{//Mando la funcion que ejectura  el hacer callback
+    var pregDificultad = [];//arreglo donde se almacenaran las pregunta con categorias y dificultad validadas
+    // Elige las preguntas validas por categoria
+    for (var i = 0; i < arreglo.length; i++) {
+      if (arreglo[i].Dificultad == dificultad) {
+        pregDificultad.push(arreglo[i]);//Agrego las preguntas validas por categorias y dificultad validadas
+      }
+    }
+    return callback(pregDificultad);//Regresa el objeto obtenido en el callback
+  })
+}
+// Creo funcion para separar por categoria
+function busquedaPreg (categoria, dificultad, arreglo, callback){
+  var pregCategorias = [];//arreglo donde se almacenaran las pregunta con categorias validadas
+  // Elige las preguntas validas por categoria
+  for (var i = 0; i < arreglo.length; i++) {
+    if (arreglo[i].Categoria == categoria) {
+      pregCategorias.push(arreglo[i]);//Agrego las preguntas validas por categoria
+    }
+  }
+  //devuelve el valor obtenido en el callback
+  return callback(dificultad, pregCategorias, (arrFin)=>{//Manda la funcion a ejecutar que hara este callback
+    var index = Math.floor((Math.random() * arrFin.length));//Elige aleatoriamente una de esas preguntas
+    return arrFin[index];//Regresa el objeto elegido
+  });
+}
+
+
+var nombreArchivoCSV = "Preguntas_computacion";
+
 fetch("./questions/"+nombreArchivoCSV+".tsv")
 .then((response)=>{
-    return response.text();
+  return response.text();
 })
 .then((tsvText)=>{
-  return tsvJSON(tsvText);
-})
-.then((json)=>{
-  var preguntas = JSON.parse(json);
-  console.log(preguntas);
+   window.preguntas = tsvOBJ(tsvText); //Declaro esta variable globalmente
 })
 .catch(error => {
-    console.error('Fallo el obtener las preguntas', error);
+  console.error('Fallo el obtener las preguntas', error);
 });
